274d2b7a84ba38c114aa5cabc1947e2cd8c698a6
Fabio Padua <fabiopadua@microsoft.com>
2023-08-31 09:46:58 -0300

    chore: listing tenants first

--- script/link-partner.ps1
+++ script/link-partner.ps1
@@ -2,11 +2,19 @@ Write-Host "Installing packages..."
 Install-Module Az.ManagementPartner
 
 Write-Host "Connecting..."
-Connect-AzAccount
+Connect-AzAccount -UseDeviceAuthentication
+
+Write-Host "Connected. Getting User's Tenants..."
+
+$tenants = Get-AzTenant;
+
+foreach ($tenant in $tenants) {
+    Write-Host "Tenant:" $($tenant.Id) "-" $($tenant.Name)
+}
 
 $partnerMPN = $null
-
-$checkID = $false
+$tenantID = $null
+$checked = $false
 
 do {
     $partnerMPN = Read-Host "Inform the Partner MPN ID"
@@ -14,69 +22,65 @@ do {
     if (![System.Int32]::TryParse($partnerMPN, [ref]$null)) {
         Write-Host "Invalid Partner MPN ID."
     } else {
-        $checkID = $true
+        $checked = $true
     }
-} until ($checkID)
+} until ($checked)
 
-$result = New-Object System.Collections.ArrayList
+$checked = $false
+do {
+    $tenantID = Read-Host "Inform the Tenant ID"
 
-Write-Host "Get user associated Tennants..."
-$tennants = Get-AzTenant;
-
-foreach ($tennant in $tennants) {
-
-    $myObject = [PSCustomObject] @{
-        Tennant = ""
-        PAL = ""
-        Subscriptions = New-Object System.Collections.ArrayList
+    if (!($tenantID -match "^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$")) {
+        Write-Host "Invalid Tenant ID."
+    } else {
+        $checked = $true
     }
+} until ($checked)
 
-    $myObject.Tennant = $($tennant.Id) + " - " + $($tennant.Name)
+$result = [PSCustomObject] @{
+    Tennant = $($tenantID)
+    PAL = ""
+    Subscriptions = ""
+}
 
-    Write-Host "Get Tennat's Subscriptions..."
+# Connecting in the current Tennant for trying change the MPN ID
+Write-Host "Connecting on Tenant " $($tenantID) "..."
+Connect-AzAccount -TenantId $($tenantID)
 
-    $subscriptionsList = New-Object System.Collections.ArrayList
+Write-Host "Get Tennat's Subscriptions..."
 
-    $subscriptions = Get-AzSubscription -TenantId $($tennant.Id)
+$subscriptionsList = @()
 
-    foreach ($subscription in $subscriptions) {
-        Write-Host "Get Subscription ID " + $($subscription.Id) + " permissions..."
-     
-        # Perform operations on the subscription here
-        $scope = "/subscriptions/" + $($subscription.Id)
-        $roleAssignments = Get-AzRoleAssignment -Scope $scope
+$subscriptions = Get-AzSubscription -TenantId $tenantID
 
-        $subscriptionObject = [PSCustomObject] @{
-            Subscription = ($subscription.Id) + " - " + ($subscription.Name)
-            UserPermissions = $roleAssignments
-        }
-
-        $subscriptionsList.Add($subscriptionObject)
-
-    }
-
-    $myObject.Subscriptions = $subscriptionsList
-
-    # Connecting in the current Tennant for trying change the MPN ID
-    Connect-AzAccount -TenantId $($tennant.Id)
-
-    # Check if the tennant already has the Partner MPN ID:
-    # $existsMPNID = Get-AzManagementPartner
-    try {
-        New-AzManagementPartner -PartnerId $partnerMPN -WhatIf
-        $myObject.PAL = "Status: Registered"
-    }
-    catch {
-        Update-AzManagementPartner -PartnerId $partnerMPN -WhatIf
-        $myObject.PAL = "Status: Already registered, updated"
-    }
+foreach ($subscription in $subscriptions) {
+    Write-Host "Get Subscription ID" $($subscription.Id) "-" $($subscription.Name) "permissions..."
     
-    Write-Host "Printing object..."
-    Write-Host $myObject
-    $result.Add($myObject)
+    # Perform operations on the subscription here
+    $scope = "/subscriptions/" + $($subscription.Id)
+    $roleAssignments = Get-AzRoleAssignment -Scope $scope
+
+    Write-Host "Permissions on Subscription ID" $($subscription.Id) ":" $($roleAssignments -join ",")
+    $subscriptionObject = [PSCustomObject] @{
+        Subscription = ($subscription.Id) + " - " + ($subscription.Name)
+        UserPermissions = $roleAssignments -join ","
+    }
+
+    $subscriptionsList += $subscriptionObject
 
 }
 
+$result.Subscriptions = $subscriptionsList -join ","
+
+# Check if the tennant already has the Partner MPN ID:
+try {
+    New-AzManagementPartner -PartnerId $partnerMPN -WhatIf
+    $result.PAL = "Status: Registered"
+}
+catch {
+    Update-AzManagementPartner -PartnerId $partnerMPN -WhatIf
+    $result.PAL = "Status: Already registered, updated"
+}
+    
 Write-Host "Printing results..."
-$resultString = $result -join "`n"
-Write-Host $resultString
+Write-Host $result
